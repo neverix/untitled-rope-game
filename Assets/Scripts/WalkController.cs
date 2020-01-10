dfdf8f9422e52522d31f53c9df66fd1643411811
Stepan Shabalin <stomperhomp@gmail.com>
2020-01-10 11:26:47 -0500

    добавил игрока

---
+++ Assets/Scripts/WalkController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkController : MonoBehaviour
+{
+    public float speed;
+    CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        characterController = GetComponent<CharacterController> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis ("Horizontal");
+        float z = Input.GetAxis ("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        move *= speed * Time.deltaTime;
+
+        characterController.Move (move);
+    }
+}
